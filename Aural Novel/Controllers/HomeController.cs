b4d82ceb6c4f3536091276f519e0fcc98f784087
Syedz68 <107263740+Syedz68@users.noreply.github.com>
2023-01-21 23:59:17 +0600

    Add project files.

---
+++ Aural Novel/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using Aural_Novel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aural_Novel.Controllers
+{
+    public class HomeController : Controller
+    {
+        AuralNovelEntities db = new AuralNovelEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ReaderSignup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ReaderSignup(Reader r)
+        {
+            if(db.Readers.Any(x => x.remail == r.remail))
+            {
+                ViewBag.Notification = "Account already exists";
+                return View();
+            }
+            else
+            {
+                db.Readers.Add(r);
+                db.SaveChanges();
+
+                Session["Rid"] = r.rid.ToString();
+                Session["Rfname"] = r.rfname.ToString();
+                Session["Rlname"] = r.rlname.ToString();
+                Session["Rusername"] = Session["Rfname"] + " " + Session["Rlname"];
+
+                return RedirectToAction("Index", "Home");
+            }
+        }
+        
+
+        public ActionResult Signout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult ReaderSignin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ReaderSignin(Reader r)
+        {
+            var detail = db.Readers.Where(u => u.remail.Equals(r.remail) && u.rpass.Equals(r.rpass)).FirstOrDefault();
+            if (detail != null)
+            {
+                Session["Rid"] = detail.rid.ToString();
+                Session["Rfname"] = detail.rfname.ToString();
+                Session["Rlname"] = detail.rlname.ToString();
+                Session["Rusername"] = Session["Rfname"] + " " + Session["Rlname"];
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Notification = "Invalid User or Password";
+            }
+            return View();
+
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Aural Novel/Controllers/SellerController.cs
@@ -0,0 +1,80 @@
+﻿using Aural_Novel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aural_Novel.Controllers
+{
+    public class SellerController : Controller
+    {
+        AuralNovelEntities db = new AuralNovelEntities();
+        // GET: Seller
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult SellerSignup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SellerSignup(Seller s)
+        {
+            if (db.Sellers.Any(x => x.semail == s.semail))
+            {
+                ViewBag.Notification = "Account already exists";
+                return View();
+            }
+            else
+            {
+                db.Sellers.Add(s);
+                db.SaveChanges();
+
+                Session["Sid"] = s.sid.ToString();
+                Session["Sfname"] = s.sfname.ToString();
+                Session["Slname"] = s.slname.ToString();
+                Session["Susername"] = Session["Sfname"] + " " + Session["Slname"];
+
+                return RedirectToAction("Index", "Seller");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult SellerSignin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SellerSignin(Seller s)
+        {
+            var detail = db.Sellers.Where(u => u.semail.Equals(s.semail) && u.spass.Equals(s.spass)).FirstOrDefault();
+            if (detail != null)
+            {
+                Session["Sid"] = detail.sid.ToString();
+                Session["Sfname"] = detail.sfname.ToString();
+                Session["Slname"] = detail.slname.ToString();
+                Session["Susername"] = Session["Sfname"] + " " + Session["Slname"];
+
+                return RedirectToAction("Index", "Seller");
+            }
+            else
+            {
+                ViewBag.Notification = "Invalid User or Password";
+            }
+            return View();
+
+        }
+        public ActionResult SellerSignout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Seller");
+        }
+
+    }
+}
